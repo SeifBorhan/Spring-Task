b482f00aac37308d17e172a817740782c62b9a3f
Seif Borhaneldin <sborhaneldin@sumerge.com>
2024-07-31 15:17:16 +0300

    SPA INIT

--- src/main/java/com/spring/task/SpringTaskApplication.java
+++ src/main/java/com/spring/task/SpringTaskApplication.java
@@ -2,12 +2,15 @@ package com.spring.task;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringTaskApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringTaskApplication.class, args);
+        ApplicationContext context = SpringApplication.run(SpringTaskApplication.class, args);
+//        CourseService service = context.getBean(CourseService.class);
+//        service.recommendCourse();
     }
 
 }
