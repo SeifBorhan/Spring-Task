94a8d06c532c1b9df70754db3665811443864aed
Seif Borhaneldin <sborhaneldin@sumerge.com>
2024-08-05 15:46:32 +0300

    Added Swagger using (Springdoc)

--- src/main/java/com/spring/task/CourseService.java
+++ src/main/java/com/spring/task/CourseService.java
@@ -9,7 +9,6 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
 @Service
 public class CourseService {
     private CourseRecommender courseRecommender;
--- src/main/java/com/spring/task/SpringTaskApplication.java
+++ src/main/java/com/spring/task/SpringTaskApplication.java
@@ -7,8 +7,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
+
 @SpringBootApplication
 public class SpringTaskApplication {
+
 
     public static void main(String[] args) {
         ApplicationContext context = SpringApplication.run(SpringTaskApplication.class, args);
