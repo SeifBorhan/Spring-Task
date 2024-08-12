4cb77007a1642633b026df401df3f697997713c5
Seif Borhaneldin <sborhaneldin@sumerge.com>
2024-08-12 17:28:27 +0300

    Branching!

--- src/main/java/com/spring/task/config/SecurityConfig.java
+++ src/main/java/com/spring/task/config/SecurityConfig.java
@@ -1,10 +1,11 @@
 package com.spring.task.config;
 
 
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -13,13 +14,14 @@ import static org.springframework.security.config.Customizer.withDefaults;
 public class SecurityConfig {
 
     @Bean
+    @Order(SecurityProperties.BASIC_AUTH_ORDER)
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
         http
-                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorizeRequests ->
                         authorizeRequests
                                 .requestMatchers("/courses/view/**").permitAll()
                                 .requestMatchers("/add", "/update/**", "/delete/**").authenticated()
+                                .anyRequest().authenticated()
                 )
                 .httpBasic(withDefaults());
         return http.build();
