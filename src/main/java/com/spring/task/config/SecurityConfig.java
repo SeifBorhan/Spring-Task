22c5dbfc16614246be8b720b168474dbdef1b042
Seif Borhaneldin <sborhaneldin@sumerge.com>
2024-08-13 11:44:54 +0300

    Done!

--- src/main/java/com/spring/task/config/SecurityConfig.java
+++ src/main/java/com/spring/task/config/SecurityConfig.java
@@ -1,21 +1,17 @@
 package com.spring.task.config;
 
 
-import jakarta.servlet.Filter;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
-import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
-
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
@@ -38,9 +34,25 @@ public class SecurityConfig {
                                 .anyRequest().authenticated()
                 )
                .addFilterBefore(customFilter, BasicAuthenticationFilter.class)
-                .httpBasic(withDefaults());
+                .httpBasic(withDefaults())
+                .authenticationProvider(daoAuthenticationProvider());
+
         return http.build();
     }
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(customUserDetailsService);
+        provider.setPasswordEncoder(passwordEncoder());
+        return provider;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+
 
 
 
