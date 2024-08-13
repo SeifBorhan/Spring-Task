5e16e628e5c17c9beefef0b796fcc98435e11099
Seif Borhaneldin <sborhaneldin@sumerge.com>
2024-08-13 11:30:26 +0300

    Done!

--- src/main/java/com/spring/task/config/SecurityConfig.java
+++ src/main/java/com/spring/task/config/SecurityConfig.java
@@ -3,50 +3,45 @@ package com.spring.task.config;
 
 import jakarta.servlet.Filter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
+@Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
     private final CustomFilter customFilter;
-    private final UserDetailsService userDetailsService;
+    private final CustomUserDetailsService customUserDetailsService;
+
 
     @Bean
-    @Order(SecurityProperties.BASIC_AUTH_ORDER)
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
 
         http
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorizeRequests ->
                         authorizeRequests
-                                .requestMatchers("/courses/view/**").permitAll()
-                                .requestMatchers("/add", "/update/**", "/delete/**").authenticated()
+                                .requestMatchers("/courses/view/{id}").permitAll()
                                 .anyRequest().authenticated()
                 )
-                .addFilterBefore(customFilter, UsernamePasswordAuthenticationFilter.class)
+               .addFilterBefore(customFilter, BasicAuthenticationFilter.class)
                 .httpBasic(withDefaults());
         return http.build();
     }
 
-
-    @Bean
-    public CustomFilter customFilter() {
-        return new CustomFilter();
-    }
-
-    @Bean
-    public UserDetailsService userDetailsService() {
-        return new CustomUserDetailsService();
-    }
 
 
 }
